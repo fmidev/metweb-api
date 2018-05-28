9e79af01ea429b99ec591e0962c63e49b4bc9fb6
Henna Kalliokoski <henna.ruohonen@helsinki.fi>
2018-05-28 13:44:33 +0300

    session endpoint

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express')
-const app = express()
+const express = require('express'),
+      app = express(),
+      bodyparser = require('body-parser')
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+app.post('/authorize', function(req, res){
+  console.log(req.body)
+})
+
+app.get('/session', (req, res) => res.send('foobar'))
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
